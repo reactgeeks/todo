c526aa88339e791c333ecf100ef28c2742195fc9
reactgeeks <shubhmsing210@gmail.com>
2020-04-06 13:02:18 +0530

    added testcases for button sizes , background and color

--- src/todo/components/__test__/button.test.js
+++ src/todo/components/__test__/button.test.js
@@ -3,6 +3,9 @@ import React from 'react';
 import Button from '../Button';
 import { shallow, configure } from "enzyme";
 import Adapter from 'enzyme-adapter-react-16';
+import renderer from 'react-test-renderer'
+import 'jest-styled-components'
+
 
 configure({ adapter: new Adapter() });
 
@@ -12,7 +15,7 @@ describe("Testcases for button", () => {
         wrapper = shallow(<Button>click here</Button>)
     })
 
-    test("check onclick function are triggered by mock functoin", () => {
+    test("test onclick function are triggered by mock functoin", () => {
         let onclick = jest.fn(() => {});
         wrapper.setProps({
             handleClick: onclick,
@@ -33,13 +36,12 @@ describe("Testcases for button", () => {
         expect(html.indexOf('Loading...')).toBe(102);
     })
 
-    test("check icons and there positions", () => {
+    test("test icons and there positions", () => {
         wrapper.setProps({
             icon: 'FaGithub',
             iconPosition: 'left'
         });
         let html = wrapper.html();
-        console.log(html)
         expect(html.indexOf('flex-direction:row')).toBe(28);
 
         wrapper.setProps({
@@ -47,7 +49,6 @@ describe("Testcases for button", () => {
             iconPosition: 'right'
         });
         html = wrapper.html();
-        console.log(html)
         expect(html.indexOf('flex-direction:row-reverse')).toBe(28);
 
         wrapper.setProps({
@@ -55,16 +56,26 @@ describe("Testcases for button", () => {
             iconPosition: 'top'
         });
         html = wrapper.html();
-        console.log(html)
         expect(html.indexOf('flex-direction:column')).toBe(28);
 
-        wrapper.setProps({
-            icon: 'FaGithub',
-            iconPosition: 'bottom'
-        });
-        html = wrapper.html();
-        console.log(html)
-        expect(html.indexOf('flex-direction:column-reverse')).toBe(28);
+        
     })
+
+    test("test background and color", () => {
+        const bg = "white";
+        const color = "black";
+        const tree = renderer.create(<Button bg={bg} color={color}/>).toJSON();
+        expect(tree).toHaveStyleRule('background', bg);
+        expect(tree).toHaveStyleRule('color', color);
+    });
+
+    test("test customizable button sizes", () => {
+        const smallButton = renderer.create(<Button size="sm"/>).toJSON();
+        expect(smallButton).toHaveStyleRule('font-size', '0.7em');
+        const normalButton = renderer.create(<Button />).toJSON();
+        expect(normalButton).toHaveStyleRule('font-size', '1em');
+        const largeButton = renderer.create(<Button size="lg"/>).toJSON();
+        expect(largeButton).toHaveStyleRule('font-size', '1.5em');
+    });
 
 })
